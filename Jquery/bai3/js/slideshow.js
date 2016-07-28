3ea20ae6d22407a14ff03cf2974bd68dce8c2bb7
Le Phan Anh Khoa <lephananhkhoa@gmail.com>
2016-07-28 11:31:26 +0700

    update bai3 jquery

---
+++ Jquery/bai3/js/slideshow.js
@@ -0,0 +1,77 @@
+﻿$(document).ready(function(){
+	var stt = 0;
+	startImg = parseInt($("img:first").attr("stt")); // get start img
+	endImg = parseInt($("img:last").attr("stt")); // get end img
+	$("img").each(function(){
+		if($(this).is(':visible')){
+			stt = parseInt(($(this).attr("stt")));
+		}
+	});
+	// next image
+	$("#next").click(function(){	
+		next = ++stt;
+		if(next == endImg){
+			stt = (startImg-1);
+		}
+		$("img").hide();
+		$("img").eq(next).show();
+		$("li").removeClass('active');
+		$("li").eq(next).addClass('active');
+	});
+	// prev image
+	$("#prev").click(function(){
+		prev = --stt;
+		if(prev == -5){
+			stt = (endImg+1);
+		}
+		$("img").hide();
+		$("img").eq(prev).show();	
+		$("li").removeClass('active');
+		$("li").eq(prev).addClass('active');
+	});
+	// click id image
+	$("#img_1").click(function(){
+		prev = --stt;
+		if(prev == -5){
+			stt = (endImg+1);
+		}
+		stt= startImg;
+		$("img").hide();
+		$("img").eq(stt).show();
+		$("li").removeClass('active');
+		$("li").eq(stt).addClass('active');
+  });
+  	$("#img_2").click(function(){
+		stt= startImg+1;
+		$("img").hide();
+		$("img").eq(stt).show();
+		$("li").removeClass('active');
+		$("li").eq(stt).addClass('active');
+  });
+  	$("#img_3").click(function(){
+		stt= startImg+2;
+		$("img").hide();
+		$("img").eq(stt).show();
+		$("li").removeClass('active');
+		$("li").eq(stt).addClass('active');
+  });
+  	$("#img_4").click(function(){
+		stt= startImg+3;
+		$("img").hide();
+		$("img").eq(stt).show();
+		$("li").removeClass('active');
+		$("li").eq(stt).addClass('active');
+  });
+  	$("#img_5").click(function(){
+		if(	stt= endImg)       // if stt =4, next stt =0
+			stt = (startImg-1);
+		$("img").hide();
+		$("img").eq(stt).show();
+		$("li").removeClass('active');
+		$("li").eq(stt).addClass('active');
+  });
+  // auto motion image
+	setInterval(function(){
+		$("#next").click();
+	},8000); // set time motion
+});
