6bce50bbd5ed833d15ff1d2e00e290472c4ab79e
Le Phan Anh Khoa <lephananhkhoa@gmail.com>
2016-08-02 16:00:41 +0700

    fix bai 2 javascript

--- Javascript/bai2/js/form.js
+++ Javascript/bai2/js/form.js
@@ -119,12 +119,9 @@ function submitAjax() {
 			//Send the proper header information along with the request
 
 			http.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); 
-			http.setRequestHeader("Content-length", params.length);   
-			http.setRequestHeader("Connection", "close");
 
 			http.onreadystatechange = function() {				//Call a function when the state changes.
 				if(http.readyState == 4 && http.status == 200) {
-					console.log(http.responseText)
 					var obj = JSON.parse(http.responseText);
 					if(obj.status == "1"){
 						errorUser.innerHTML = "Username is avaiable \n";
