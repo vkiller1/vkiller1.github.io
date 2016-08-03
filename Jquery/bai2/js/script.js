8caf9017feb01cc4a2c1b655b095d19d144027b8
Le Phan Anh Khoa <lephananhkhoa@gmail.com>
2016-08-03 17:34:38 +0700

    fix exercise

--- Jquery/bai2/js/script.js
+++ Jquery/bai2/js/script.js
@@ -1,47 +1,123 @@
-function checkForm() {
+function getInfo() {
 // Fetching values from all input fields and storing them in variables.
-	var username = $("#username1").value;
-	var password = $("#password1").value;
-	var email = $("#email1").value;
-	var birthday = $("#birthday1").value;
-//Check input Fields Should not be blanks.
-	if (username == '' || password == '' || email == '' || birthday == '') {
-		alert("Fill All Fields");
-	} else {
-//Notifying error fields
-	var username1 = $("#username").value;
-	var password1 = $("#password").value;
-	var email1 = $("#email").value;
-	var birthday1 = $("#birthday").value;
-//Check All Values/Informations Filled by User are Valid Or Not.If All Fields Are invalid Then Generate alert.
-	if (username1.innerHTML == 'Must be 3+ letters' || password1.innerHTML == 'Password too short' || email1.innerHTML == 'Invalid email') {
-		alert("Fill Valid Information");
-	} else { if(username1.innerHTML == null || password1.innerHTML == null || email1.innerHTML == null){
-		alert(" Please fill full information");
-	}
-//Submit Form When All values are valid.
-		$("#myForm").submit();
-		alert(" Submit Success");
-	}
-	}
+	
+	email = $("#email1").val();
+	$birthday = $("#birthday1").val();
+	errorUser = $("#errorUser");
+	errorPassword = $("#errorPassword");
+	errorEmail = $("#errorEmail");
 }
-// AJAX code to check input field values when onblur event triggerd.
-function validate(field, query) {
-	var xmlhttp;
-	if (window.XMLHttpRequest) { // for IE7+, Firefox, Chrome, Opera, Safari
-	xmlhttp = new XMLHttpRequest();
-	} else { // for IE6, IE5
-		xmlhttp = $ActiveXObject("Microsoft.XMLHTTP");
+//check validate username
+function validateUsername() {
+    getInfo();
+    var illegalChars = /\W/; // allow letters, numbers, and underscores
+    if ($("#username1").val() == "") {
+        $("#username1").css('background-color','yellow');
+        errorUser.text("You didn't enter a username.\n");
+        return false;
+    } else if ( $("#username1").val().length < 8 ) {
+        	$("#username1").css('background-color','yellow');
+        	errorUser.text("Username length min 8 letter.\n");
+		return false;
+    } else if (illegalChars.test($("#username1").val())) {
+        	$("#username1").css('background-color','yellow');
+        	errorUser.text("The username contains illegal characters.\n");
+		return false;
+    } else {		
+        $("#username1").css('background-color','white');
+	    errorUser.text("");
+    }
+    return true;
+}
+//check validate password
+function validatePassword() {
+    var illegalChars = /\W/; // allow letters, numbers, and underscores
+    if ($("#password1").val() == "") {
+		 $("#password1").css('background-color','yellow');
+         errorPassword.text("You didn't enter password .\n");
+        return false;
+    } else if (($("#password1").val().length < 7) || ($("#password1").val().length > 15)) {
+        $("#password1").css('background-color','yellow');
+        errorPassword.text("The password is the wrong length. \n");
+        return false;
+    } else if ( ($("#password1").val().search(/[a-zA-Z]+/)==-1) || ($("#password1").val().search(/[0-9]+/)==-1) ) {
+        $("#password1").css('background-color','yellow');
+        errorPassword.text("The password must contain at least one numeral.\n");
+        return false;
+    } else {
+        $("#password1").css('background-color','while');
+		errorPassword.text("");
+    }
+   return true;
+}
+//check validate email
+function validateEmail() {                     // value of field with whitespace trimmed off
+    //var temail = $("#email1").html().replace(/^\s+|\s+$/, '');
+	var temail = $("#email1").val();
+    $("#email1").html(temail);
+    //var emailFilter = /^[^@]+@[^@.]+\.[^@]*\w\w$/ ;
+	var emailFilter = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+    var illegalChars= /[\(\)\<\>\,\;\:\\\"\[\]]/ ;
+    if ($("#email1").val() == "") {
+        $("#email1").css('background-color','yellow');
+        errorEmail.text("You didn't enter an email address.\n");
+	return false;
+    } else if (!emailFilter.test(temail)) {              //test email for illegal characters
+        $("#email1").css('background-color','yellow');
+        errorEmail.text("Please enter a valid email address.\n");
+	return false;
+    } else if (temail.match(illegalChars)) {
+        $("#email1").css('background-color','yellow');
+        errorEmail.text("The email address contains illegal characters.\n");
+	return false;
+    } else {
+        $("#email1").css('background-color','while');
+        errorEmail.text(" ");
+    }
+    return true;
+}
+function submitAjax() {
+	var isValidUsername = validateUsername() ;
+	var isValidatePassword = validatePassword() ;
+	var isValidateEmail =validateEmail() ;
+		if(isValidUsername && isValidatePassword && isValidateEmail) {
+		  $.ajax({
+			 url : "server.php",
+			 type : "POST",
+			 dataType : "json",
+			 data :{
+				 username : $('input[name="username"]').val(),
+				 password : $('input[name="password"]').val(),
+				 email : $('input[name="email"]').val(),
+				 birthday : $('input[name="birthday"]').val()
+			 },
+			 success:function(data){
+				 console.log(data);
+				 if(data.status == "1"){
+					errorUser.text("Username is avaiable \n");
+				}else{
+					alert(" Register Successfull" );
+				}
+			 }
+		  }); 
+	}		
+}
+// use switch case reset form
+function clearForm(oForm) {
+  var elements = oForm.elements; 
+  oForm.reset();
+  for(i=0; i<elements.length; i++) {
+	field_type = elements[i].type.toLowerCase();
+	switch(field_type) {
+		case "text": 
+		case "password": 
+		case "textEmail":
+	        case "hidden":	
+	        case "textBirthDay":			
+			elements[i].value = ""; 
+			break;
+		default: 
+			break;
 	}
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState != 4 && xmlhttp.status == 200) {
-			document.getElementById(field).innerHTML = "Validating..";
-		} else if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-			document.getElementById(field).innerHTML = xmlhttp.responseText;
-		} else {
-			document.getElementById(field).innerHTML = "Error Occurred. <a href='index.html'>Reload Or Try Again</a> the page.";
-		}
-	}
-	xmlhttp.open("GET", "validation.php?field=" + field + "&query=" + query, false);
-	xmlhttp.send();
+    }
 }
---
+++ Jquery/bai2/server.php
@@ -0,0 +1,15 @@
+<?php
+ 
+	$arr = array("lephananhkhoa","lephananhkhoa@gmail.com");
+
+	if(in_array($_POST['username'], $arr)){
+
+		echo json_encode(array('status'=>"1"));
+
+	}else{
+
+		echo json_encode(array('status'=>"0"));
+
+	}
+
+?>
